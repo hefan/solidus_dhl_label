aa1528907fd6f657efa9b1e47d3f1d2974478d26
stefan <stefan@yo-code.de>
2024-05-07 13:01:22 +0200

    open label in new tab

--- app/overrides/spree/admin/orders/edit/add_dhl_label_button.rb
+++ app/overrides/spree/admin/orders/edit/add_dhl_label_button.rb
@@ -4,7 +4,7 @@ Deface::Override.new(
   insert_top: ".page-actions",
   text: '
     <% if @order and @order.completed_at? %>
-      <li><%= link_to(I18n.t("spree.dhl_label.title"), "/admin/orders/"+@order.number+"/dhl-label", class: "button fa fa-tag") %></li>
+      <li><%= link_to(I18n.t("spree.dhl_label.title"), "/admin/orders/"+@order.number+"/dhl-label", target: "_blank", class: "button fa fa-tag") %></li>
     <% end %>
   ',
   disabled: false
